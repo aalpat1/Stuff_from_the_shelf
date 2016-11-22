758f1f8a1935454ffe4d7a9f1082ca2580984ccf
aalpat <apatankar@media.ucla.edu>
2016-11-22 01:28:55 -0500

    Final version

--- JS/application.js
+++ JS/application.js
@@ -1,47 +1,47 @@
- (function() {
+ // (function() {
             
-            var windowH = $(window).height(),
-                documElem = $(document),
-                landingPage = $('.landingPage'),
-                article = $('.article'),
-                articleContent= $('#articleContent'),
-                Headline= $('#Headline'),
-                albumCover= $('#albumCover'),
-                playlist= $('#playlist'),
-                btns = $('.btn'),
-                animSpeed = 500;
-            var article1Headline= "An Argument With Myself";
-            landingPage.css({
-                height: windowH + 'px',
-                top: 0 + 'px'
-            });
+ //            var windowH = $(window).height(),
+ //                documElem = $(document),
+ //                landingPage = $('.landingPage'),
+ //                article = $('.article'),
+ //                articleContent= $('#articleContent'),
+ //                Headline= $('#Headline'),
+ //                albumCover= $('#albumCover'),
+ //                playlist= $('#playlist'),
+ //                btns = $('.btn'),
+ //                animSpeed = 500;
+ //            var article1Headline= "An Argument With Myself";
+ //            landingPage.css({
+ //                height: windowH + 'px',
+ //                top: 0 + 'px'
+ //            });
 
-            article.css({
-                top: windowH + 'px',
-            });
-           $('#article1').on('click', function(e) {
-                    playlist.load("Assets/Multimedia/article1playlist.html")
-                    Headline.html(article1Headline);
-                    albumCover.load("Assets/Art/article1image.html");
-                    articleContent.load("Assets/articleText/article1.html");
+ //            article.css({
+ //                top: windowH + 'px',
+ //            });
+ //           $('#article1').on('click', function(e) {
+ //                    playlist.load("Assets/Multimedia/article1playlist.html")
+ //                    Headline.html(article1Headline);
+ //                    albumCover.load("Assets/Art/article1image.html");
+ //                    articleContent.load("Assets/articleText/article1.html");
 
-                    landingPage.animate({'top': -windowH + 'px'}, {duration: animSpeed, queue: false});
-                    article.animate({'top': 0},{duration: animSpeed, queue: false} );
-           });
+ //                    landingPage.animate({'top': -windowH + 'px'}, {duration: animSpeed, queue: false});
+ //                    article.animate({'top': 0},{duration: animSpeed, queue: false} );
+ //           });
 
-        btns.on('click', function(e) {
-                if ( $(this).hasClass('open') ) {
-                    article.animate({'top': windowH + 'px'}, {duration: animSpeed, queue: false});
-                    landingPage.animate({'top': 0}, {duration: animSpeed, queue: false});
-                }
-                e.preventDefault();
-            });
+ //        btns.on('click', function(e) {
+ //                if ( $(this).hasClass('open') ) {
+ //                    article.animate({'top': windowH + 'px'}, {duration: animSpeed, queue: false});
+ //                    landingPage.animate({'top': 0}, {duration: animSpeed, queue: false});
+ //                }
+ //                e.preventDefault();
+ //            });
             
             
             
             
             
-        })();
+ //        })();
         
         
    
